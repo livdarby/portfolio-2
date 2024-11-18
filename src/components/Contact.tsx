7336283e80287b6c49a2ddfb3c22a270bcd51ceb
Olivia Darby <oliviadarby@live.co.uk>
2024-11-18 15:58:29 +0800

    contact me form:

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,15 +1,66 @@
-import { useRef } from "react";
-import emailjs from '@emailjs/browser'
+import { SyntheticEvent, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
-  
-  const form = useRef()
+  const { VITE_SERVICE_ID, VITE_TEMPLATE_ID, VITE_PUBLIC_KEY } = import.meta
+    .env;
+  const form: any = useRef();
+  const [buttonClicked, setButtonClicked] = useState<true | false>(false);
+  const [formData, setFormData] = useState<any>({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [submitButtonDisabled, setSubmitButtonDisabled] = useState<
+    true | false
+  >(true);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  console.log(formData);
+  // console.log(submitButtonDisabled)
 
-  const sendEmail = (e: any) => {e.preventDefault()
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+    VITE_SERVICE_ID &&
+      VITE_TEMPLATE_ID &&
+      form.current &&
+      emailjs
+        .sendForm(VITE_SERVICE_ID, VITE_TEMPLATE_ID, form.current, {
+          publicKey: VITE_PUBLIC_KEY,
+        })
+        .then(
+          () => {
+            console.log("Success!");
+            setButtonClicked(false);
+            setErrorMessage(
+              "Your email has been successfully sent. I'll get back to you as soon as possible!"
+            );
+          },
+          (error) => {
+            console.log("Failed!", error.text);
+            setErrorMessage("There has been an error, please try again.");
+            setButtonClicked(false);
+          }
+        );
+    setButtonClicked(true);
+    setSubmitButtonDisabled(true);
+    setFormData({
+      name: "",
+      email: "",
+      message: "",
+    });
+  };
 
-    emailjs.sendForm()
+  function handleChange(e: any) {
+    setErrorMessage("");
+    const formDataCopy = structuredClone(formData);
+    // find the e.target.name from the formDataCopy
+    formDataCopy[e.target.name] = e.target.value;
+    setFormData(formDataCopy);
+    console.log(formDataCopy.name, formDataCopy.email, formDataCopy.message);
+    formDataCopy.name && formDataCopy.email && formDataCopy.message
+      ? setSubmitButtonDisabled(false)
+      : setSubmitButtonDisabled(true);
   }
-
 
   return (
     <>
@@ -23,32 +74,98 @@ function Contact() {
           </h1>
         </div>
         <div>
-          <p className="font-body w-[75%] mx-auto text-center">
+          <p className="font-body w-[75%] mx-auto text-center mb-5">
             Interested in working together, have a question or just looking to
             say hi? Please do get in touch!
           </p>
-          <div className="underline my-12 font-body font-medium text-center flex flex-col gap-3">
-            <a
-              href="https://www.linkedin.com/in/olivia--darby/"
-              target="_blank"
-              className="cursor-pointer"
+        </div>
+        <div className="md:flex md:justify-center">
+          <div className="md:w-[65%] lg:w-[600px]">
+            <form
+              ref={form}
+              onSubmit={sendEmail}
+              className="font-body bg-[#fcecf1] flex flex-col items-center gap-4 w-[90%] mx-auto rounded py-5 md:h-[400px] md:justify-center"
             >
-              LinkedIn
-            </a>
-            <a
-              href="https://github.com/livdarby"
-              target="_blank"
-              className="cursor-pointer"
-            >
-              GitHub
-            </a>
-            <a
-              href="mailto:oliviadarby@live.co.uk?subject=Portfolio%20questions"
-              target="_blank"
-              className="cursor-pointer"
-            >
-              Email
-            </a>
+              <div className="w-max flex gap-5 justify-center">
+                <label className="w-[90px] flex justify-end items-center">
+                  Name :
+                </label>
+                <input
+                  className="text-sm w-[250px] md:w-[330px]"
+                  onChange={handleChange}
+                  value={formData.name}
+                  type="text"
+                  name="name"
+                />
+              </div>
+              <div className="w-max flex gap-5 justify-center">
+                <label className="w-[90px] flex justify-end items-center">
+                  Email :
+                </label>
+                <input
+                  className="text-sm w-[250px] md:w-[330px]"
+                  onChange={handleChange}
+                  value={formData.email}
+                  type="email"
+                  name="email"
+                />
+              </div>
+              <div className="w-max flex gap-5 justify-center">
+                <label className="w-[90px] flex justify-end items-center">
+                  Message :
+                </label>
+                <textarea
+                  className="text-sm w-[250px] md:w-[330px]"
+                  onChange={handleChange}
+                  value={formData.message}
+                  name="message"
+                />
+              </div>
+              <input
+                type="submit"
+                value="Send"
+                disabled={submitButtonDisabled}
+                className={
+                  submitButtonDisabled
+                    ? `text-white cursor-not-allowed`
+                    : `text-black cursor-pointer`
+                }
+              />
+              {buttonClicked && <span className="loader"></span>}
+              {errorMessage && (
+                <p className="text-center font-body text-sm px-3">
+                  {errorMessage}
+                </p>
+              )}
+            </form>
+          </div>
+          <div className="md:w-[30%] md:flex md:flex-col md:justify-center">
+            <p className="text-center font-body mt-5">
+              Alternatively, you can reach me on
+            </p>
+            <div className="underline my-5 font-body font-medium text-center flex flex-col gap-3">
+              <a
+                href="https://www.linkedin.com/in/olivia--darby/"
+                target="_blank"
+                className="cursor-pointer"
+              >
+                LinkedIn
+              </a>
+              <a
+                href="https://github.com/livdarby"
+                target="_blank"
+                className="cursor-pointer"
+              >
+                GitHub
+              </a>
+              <a
+                href="mailto:oliviadarby@live.co.uk?subject=Portfolio%20questions"
+                target="_blank"
+                className="cursor-pointer"
+              >
+                Email
+              </a>
+            </div>
           </div>
         </div>
       </section>
--- vite.config.ts
+++ vite.config.ts
@@ -6,3 +6,15 @@ export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.JPG"],
 });
+
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SERVICE_ID: string;
+  readonly VITE_TEMPLATE_ID: string;
+  readonly VITE_PUBLIC_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
